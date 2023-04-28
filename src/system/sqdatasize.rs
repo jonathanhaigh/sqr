8f731397ebb78c0a1243bd54a6d1511020f39d92
Jonathan Haigh <jonathanhaigh@gmail.com>
2023-04-28 16:38:22 +0100

    Add SqDataSize unit conversion fields

--- src/system/sqdatasize.rs
+++ src/system/sqdatasize.rs
@@ -1,7 +1,18 @@
 use anyhow::anyhow;
 
 use crate::primitive::Primitive;
-use crate::system::SqDataSizeTrait;
+use crate::system::{sqfloat::SqFloat, sqint::SqInt, SqDataSizeTrait};
+
+const KIB: f64 = 1024f64;
+const KB: f64 = 1000f64;
+const MIB: f64 = 1024f64 * KIB;
+const MB: f64 = 1000f64 * KIB;
+const GIB: f64 = 1024f64 * MIB;
+const GB: f64 = 1000f64 * MIB;
+const TIB: f64 = 1024f64 * GIB;
+const TB: f64 = 1000f64 * GIB;
+const PIB: f64 = 1024f64 * TIB;
+const PB: f64 = 1000f64 * TIB;
 
 pub struct SqDataSize {
     value: u64,
@@ -22,5 +33,55 @@ impl SqDataSizeTrait for SqDataSize {
             )),
             Ok(value_i64) => Ok(Primitive::Int(value_i64)),
         }
+    }
+
+    fn B(&self) -> anyhow::Result<SqInt> {
+        match i64::try_from(self.value) {
+            Ok(value_i64) => Ok(SqInt::new(value_i64)),
+            Err(_) => Err(anyhow!(
+                "Failed to convert data size {}B to 64-bit signed integer",
+                self.value
+            )),
+        }
+    }
+
+    fn KiB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / KIB))
+    }
+
+    fn kB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / KB))
+    }
+
+    fn MiB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / MIB))
+    }
+
+    fn MB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / MB))
+    }
+
+    fn GiB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / GIB))
+    }
+
+    fn GB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / GB))
+    }
+
+    fn TiB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / TIB))
+    }
+
+    fn TB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / TB))
+    }
+
+    fn PiB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / PIB))
+    }
+
+    fn PB(&self) -> anyhow::Result<SqFloat> {
+        Ok(SqFloat::new(self.value as f64 / PB))
     }
 }
